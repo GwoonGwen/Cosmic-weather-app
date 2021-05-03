75df75a2888eb97d8aaec4b1199aa5f24a41bee9
GwoonGwen <74727367+GwoonGwen@users.noreply.github.com>
2021-05-03 21:39:35 +0200

    added id's and first part city input function

--- src/app.js
+++ src/app.js
@@ -36,7 +36,27 @@ function currentTime(time) {
     return `${hour}:${minutes}`;
 }
 
+function cityInputWeather(response) {
+    document.querySelector("#city").innerHTML = response.data.name;
+    document.querySelector("")
+}
+
+function cityUserInput(event) {
+    event.preventDefault();
+    let cityInput = document.querySelector("#search-bar").nodeValue.trim();
+    let apikey = "b6ea7199b1cb9aca54197fcbaab59e85";
+    let units = "metric";
+    let mainUrl = "https://api.openweathermap.org/data/2.5/weather?";
+    let apiUrl = `${mainUrl}q=${cityInput}&appid=${apiKey}&units=${units}`;
+    axios.get(apiUrl).then(cityInputWeather)
+}
+
 let now = new Date();
 let dateToday = document.querySelector("#date").innerHTML = currentDate(now);
 let timeToday = document.querySelector("#time").innerHTML = currentTime(now);
 
+let cityInput = document.querySelector("#search-bar");
+cityInput.addEventListener("submit", cityUserInput);
+
+let pinpoint = document.querySelector("#pinpoint");
+pinpoint.addEventListener("click", pinpointCity);
