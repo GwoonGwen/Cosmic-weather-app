0cebc2d199dbd5586b881b384ec2a1b83f9b3d9c
GwoonGwen <74727367+GwoonGwen@users.noreply.github.com>
2021-05-10 12:39:06 +0200

    forecast JS function added

--- src/app.js
+++ src/app.js
@@ -29,6 +29,43 @@ let thisMonth = months[date.getMonth()];
 return `${today} ${dateNr} ${thisMonth}`;
 }
 
+function displayForecast() {
+    let forecastElement = document.querySelector("#forecast");
+    let forecastHTML = `<div class="row">`;
+    let days = ["Tue", "Wed", "Thu", "Fri"];
+    days.forEach(function (day) {
+        forecastHTML = forecastHTML +
+            `
+                    <div class="col-2">
+                    <div class="forecastDay">
+                    ${day}
+                    </div>
+                    <img class="forecastEmo"
+                    src="http://openweathermap.org/img/wn/04d@2x.png"
+                    alt=""
+                    width="60"
+                    />
+                    <div 
+                    class="forecastTemp" 
+                    id="forecast-temp">
+                    <span
+                    class="forecastTempDay"
+                    id="forecast-temp-day-max">
+                    18º</span> 
+                    <span 
+                    class="forecastTempNight"
+                    id="forecast-temp-night-min">
+                    12º
+                    </span>
+                    </div>
+                </div>
+            `;
+    })
+    forecastHTML = forecastHTML + `</div>`;
+    forecastElement.innerHTML = forecastHTML;
+
+}
+
 function currentTime(time) {
     let hour = time.getHours();
     let minutes = time.getMinutes();
@@ -36,13 +73,14 @@ function currentTime(time) {
     return `${hour}:${minutes}`;
 }
 
-function cityInputWeather(response) {
+function displayWeather(response) {
     document.querySelector("#city").innerHTML = response.data.name;
     document.querySelector("#humidity-value").innerHTML = Math.round(response.data.main.humidity);
     document.querySelector("#km").innerHTML = Math.round(response.data.wind.speed);
     document.querySelector("#sky").innerHTML = response.data.weather[0].description;
     document.querySelector("#temp-main").innerHTML = Math.round(response.data.main.temp);
     document.querySelector("#icon-today").setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+
     celsiusTemp = response.data.main.temp;
 }
 
@@ -53,7 +91,7 @@ function cityUserInput(event) {
     let units = "metric";
     let mainUrl = "https://api.openweathermap.org/data/2.5/weather?";
     let apiUrl = `${mainUrl}q=${cityInput}&appid=${apiKey}&units=${units}`;
-    axios.get(apiUrl).then(cityInputWeather)
+    axios.get(apiUrl).then(displayWeather)
 }
 
 function searchPosition(position) {
@@ -64,7 +102,7 @@ function searchPosition(position) {
     let mainUrl = "https://api.openweathermap.org/data/2.5/weather?";
     let apiUrl = `${mainUrl}lat=${lat}&lon=${lon}&appid=${apiKey}&units=${units}`;
 
-    axios.get(apiUrl).then(cityInputWeather)
+    axios.get(apiUrl).then(displayWeather)
 }
 
 function pinpointCity(event) {
@@ -101,7 +139,7 @@ let pinpoint = document.querySelector("#pinpoint");
 pinpoint.addEventListener("click", pinpointCity);
 
 let cityInput = document.querySelector("#user-input");
-cityInput.addEventListener("submit", cityUserInput);
+cityInput.addEventListener("submit", displayWeather);
 
 let celsiusTemp = null;
 
@@ -110,3 +148,5 @@ fahnrenheitLink.addEventListener("click", showFahrenheit);
 
 let celsiusLink = document.querySelector("#celsius");
 celsiusLink.addEventListener("click", showCelsius);
+
+displayForecast();
